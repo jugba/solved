ad8a852f8e8be384a3992ca5e4d2b6990b4892a6
Joshua Ugba <justjosh001@gmail.com>
2019-09-14 23:19:03 +0100

    Done 14-09-19

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 
-print('Here')
+"""
+Hi, here's your problem today. This problem was recently asked by Uber:
+
+Given a list of numbers, find if there exists a pythagorean triplet in that list. A pythagorean triplet is 3 variables a, b, c where a^2 + b^2 = c^2
+
+Example:
+Input: [3, 5, 12, 5, 13]
+Output: True
+Here, 5^2 + 12^2 = 13^2.
+
+"""
+
+def findPythagoreanTriplets(nums):
+  A = {}
+  size = len(nums)
+  for i in range(size):
+    p = nums[i] ** 2
+    A[p] = 1
+    nums[i] = p
+
+  for i, val in enumerate(nums):
+    j = i + 1
+    while j < size:
+      t = nums[i] + nums[j]
+      if A.get(t):
+        return True
+      j += 1
+
+  return False
+
+print(findPythagoreanTriplets([3, 12, 5, 13]))
+# True
---
+++ september_19/14.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python
+"""
+Hi, here's your problem today. This problem was recently asked by Twitter:
+
+You are given an array of integers. Find the maximum sum of all possible contiguous subarrays of the array.
+
+Example:
+
+[34, -50, 42, 14, -5, 86]
+
+Given this input array, the output should be 137. The contiguous subarray with the largest sum is [42, 14, -5, 86].
+
+Your solution should run in linear time.
+
+Here's a starting point:
+"""
+
+def maximumSum(arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        size = len(arr)
+        if size == 0:
+            return 0
+        max_sum = arr[0]
+        curr_max = arr[0]
+        
+        dele = None
+        has_rem = False
+        
+        for i in range(1, size):
+            curr_max += arr[i]
+            if not has_rem and arr[i] < 0:
+                curr_max -= arr[i]
+                has_rem = True
+                dele = arr[i]
+            elif has_rem and arr[i] < 0 and arr[i] < dele:
+                curr_max += dele
+                curr_max -= arr[i] 
+                dele = arr[i]
+            if curr_max < arr[i]:
+                curr_max = arr[i]
+                has_rem = None
+            if curr_max > max_sum:
+                max_sum = curr_max
+        return max_sum
+
+# maximumSum([8,-1,6,-7,-4,5,-4,7,-6])
+maximumSum([11,-10,-11,8,7,-6,9,4,11,6,5,0])
+
+
+def max_subarray_sum(arr):
+  size = len(arr)
+  if size == 0:
+    return 0
+  max_sum = arr[0]
+  curr_max = arr[0]
+
+  for i in range(1, size):
+    curr_max += arr[i]
+    if curr_max > max_sum:
+      max_sum = curr_max
+    if curr_max < arr[i]:
+      curr_max = arr[i]
+  return max_sum
+    
+  
+
+# print( max_subarray_sum([34, -50, 42, 14, -5, 86]))
+# 137
